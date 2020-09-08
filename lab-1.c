04e3a0ef1e48696783102d980e55730a21cdbe0c
Alexandr <47202208+SanchesAJ@users.noreply.github.com>
2020-09-08 13:56:35 +0700

    Add files via upload

---
+++ lab-1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+
+//Поток / нить - это наименьшая еденица планирования ("Наивное" определение)
+//Поток  - нечто что создаёт иллюзию параллельного исполнения.(Определение с лекции)
+#define ERROR_TREAD_CREATE 1
+
+void printLines(char * value){
+	for (int i = 0; i < 10; ++i) {
+		printf("%s\n", value);
+	}
+}
+
+void *work(void *arg) {
+	printLines("I'm a child");
+
+	pthread_exit(NULL);
+}
+
+int main() {
+	pthread_t thread;
+	int error;
+	
+	if (error = pthread_create(&thread, NULL, &work, NULL)) {
+		char *errorLine = strerror(error);
+		fprintf(stderr,"Cannot create thread: %s\n",errorLine);
+	
+		return ERROR_TREAD_CREATE;
+	}
+	printLines("I'm a parent");
+
+	
+	pthread_exit(NULL);
+}
+
+
