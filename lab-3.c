09503119e815d82576272700d77bde267aad240f
sanches185 <sanches185@gmail.com>
2020-09-15 15:11:40 +0700

    m

--- lab-3.c
+++ lab-3.c
@@ -5,7 +5,10 @@
 
 #define ERROR_TREAD_CREATE 1
 #define ALL_RIGHT 0
+#define COUNT_OF_TREADS 4
+#define COUNT_OF_STRING 4
 
+const char *str[COUNT_OF_STRING] = {"String1", "String2", "String3", "String4"};
 
 void printTreadError(int errCode, char * comment){
 	char *errorLine = strerror(errCode);
@@ -13,24 +16,22 @@ void printTreadError(int errCode, char * comment){
 }
 
 void *work(void *arg) {
+	
 	//Используем pthread_self для получение идентификатора потока. Поскльку pthread_t - это фактически ручка(дескриптор), мы можем преобразовать его к числу.
 	unsigned long int id = pthread_self();
 	char **str = (char**)arg;
-	sleep(2);
-	printf("Thread %u: %s (%p)\n",id, str[id % 4], str[id%4]);
+	printf("Thread %u: %s %s %s\n",id, str[id % COUNT_OF_STRING], str[(id+COUNT_OF_TREADS+1)%COUNT_OF_STRING],str[(COUNT_OF_TREADS-id)%COUNT_OF_STRING]);
 	pthread_exit(NULL);
 
 }
 
 int main() {
-	pthread_t thread[4];
-	char *str[4] = {"String1", "String2", "String3", "String4"};
+	pthread_t thread[COUNT_OF_TREADS];
 	int i = 0;
-	printf("%p\n%p\n",str, &i);
 
 	int error;
 	
-	for (i = 0; i < 4; ++i){
+	for (i = 0; i < COUNT_OF_TREADS; ++i){
 		error = pthread_create(&thread[i], NULL, &work, str);
 		if (error != ALL_RIGHT) {
 			printTreadError(error,"Cannot create thread");
