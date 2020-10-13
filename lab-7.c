5ee3a1340cebc5784ee4f38e88f5c1c343a935c8
sanches185 <sanches185@gmail.com>
2020-10-13 11:47:18 +0700

    today

--- lab-7.c
+++ lab-7.c
@@ -13,100 +13,100 @@
 #define BUF_SIZE 256
 
 void printTreadError(int errCode, char * comment){
-	char *errorLine = strerror(errCode);
-	fprintf(stderr,"%s: %s\n",comment, errorLine);
+        char *errorLine = strerror(errCode);
+        fprintf(stderr,"%s: %s\n",comment, errorLine);
 }
 
 
 typedef struct data {
-	long double res;
-	int id;
-	int N;
-	int ITER;
-	
+        double res;
+        int id;
+        int N;
+        int ITER;
+
 } data;
 
 
 void *work(void *arg) {
-	data *res = (data *)arg;
-	int i = res->id * res->ITER/res->N;
-	int max_i = i + res->ITER/res->N;
-	double a = 1; double b = 1;
-	for (; i < max_i; ++i) {
-		a = (2*2*i + 1);
-		b = (2*(2*i+1) + 1);
-		res->res += (1/(a)) - (1/(b));
-	}
-	pthread_exit(NULL);
+        data *res = (data *)arg;
+        int i = res->id * res->ITER/res->N;
+        int max_i = i + res->ITER/res->N;
+        double a = 1;double b = 1;
+        for (; i < max_i; ++i) {
+                a = (2*2*i + 1);
+                b = (2*(2*i+1) + 1);
+                res->res += (1/(a)) - (1/(b));
+        }
+        pthread_exit(NULL);
 }
 
 
 int main(int argc, char **argv) {
-	pthread_t *thread = NULL;
-	data *results = NULL;
-	double PI = 0;
-	pthread_attr_t thread_attr;
-	int N = 0; 
-	int ITER = 0;
-	int i = 0;
-	int error;
+        pthread_t *thread = NULL;
+        data *results = NULL;
+        double PI = 0;
+        pthread_attr_t thread_attr;
+        int N = 0;
+        int ITER = 0;
+        int i = 0;
+        int error;
 
-	if (argc < 3) {
-		perror("Too few arguments");
-		return INCORRECT_ARGS;
-	}
+        if (argc < 3) {
+                perror("Too few arguments");
+                return INCORRECT_ARGS;
+        }
 
-	N = atoi(argv[1]);
-	ITER = atoi(argv[2]);
-	if (N <= 0) {
-		perror("Incorrect number of threads");
-		return INCORRECT_ARGS;
-	}
-	if (ITER <= 0) {
-		perror("Incorrect number of iterations");
-		return INCORRECT_ARGS;
-	}
+        N = atoi(argv[1]);
+        ITER = atoi(argv[2]);
+        if (N <= 0) {
+                perror("Incorrect number of threads");
+                return INCORRECT_ARGS;
+        }
+        if (ITER <= 0) {
+                perror("Incorrect number of iterations");
+                return INCORRECT_ARGS;
+        }
 
-	
-	thread = (pthread_t*)malloc(sizeof(pthread_t)*N);
-	if (thread== NULL) {
-		perror("couldn't allocate memory");
-		return ALLOCATOR_ERROR;
-	}
-	
-	results = (data*)malloc(sizeof(data)*N);
-	if (results == NULL) {
-		perror("couldn't allocate memory");
-		return ALLOCATOR_ERROR;
-	}
 
-	for (i = 0; i < N; ++i) {
-		results[i].res = 0;
-		results[i].id = i;
-		results[i].N = N;
-		results[i].ITER = ITER;
-		error = pthread_create(&(thread[i]), NULL, &work, results + i);
-		if (error!=ALL_RIGHT) {
-			printTreadError(error,"couldn't create thread");
-			return ERROR_TREAD_CREATE;
-		}
-	}
+        thread = (pthread_t*)malloc(sizeof(pthread_t)*N);
+        if (thread== NULL) {
+                perror("couldn't allocate memory");
+                return ALLOCATOR_ERROR;
+        }
 
-	for (i = 0; i < N; ++i) {
-		error = pthread_join(thread[i], NULL);
-		if (error != ALL_RIGHT) {
-			perror("couldn't join threads");
-			return ERROR_TREAD_JOIN;
-		}
-		PI += results[i].res;
-	}
-	
-	free(results);
-	PI *= 4;
-	printf("PI = %.8lf\n",PI);
-	
-	
-	pthread_exit(NULL);
+        results = (data*)malloc(sizeof(data)*N);
+        if (results == NULL) {
+                perror("couldn't allocate memory");
+                return ALLOCATOR_ERROR;
+        }
+
+        for (i = 0; i < N; ++i) {
+                results[i].res = 0;
+                results[i].id = i;
+                results[i].N = N;
+                results[i].ITER = ITER;
+                error = pthread_create(&(thread[i]), NULL, &work, results + i);
+                if (error!=ALL_RIGHT) {
+                        printTreadError(error,"couldn't create thread");
+                        return ERROR_TREAD_CREATE;
+                }
+        }
+
+        for (i = 0; i < N; ++i) {
+                error = pthread_join(thread[i], NULL);
+                if (error != ALL_RIGHT) {
+                        perror("couldn't join threads");
+                        return ERROR_TREAD_JOIN;
+                }
+                PI += results[i].res;
+        }
+
+        free(results);
+        PI *= 4;
+        printf("PI = %.16lf\n",PI);
+
+
+        pthread_exit(NULL);
 }
 
 
--- lab-8.c
+++ lab-8.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <string.h>
 #include <unistd.h>
+#include <signal.h>
 #include <stdlib.h>
+#include <string.h>
+void *routine(void *arg);
+
+int N = 0;
+int alive;
+int ITER = 0;
+pthread_barrier_t barrier;
+int inconst = 0;
+pthread_mutex_t mutex;
+pthread_cond_t cond;
+
 
 #define INCORRECT_ARGS -3
 #define ALLOCATOR_ERROR -4
@@ -19,165 +30,157 @@
 #define ALL_RIGHT 0
 #define COUNT_OF_TREADS 4
 
-int N = 0; 
-int alive;
-int ITER = 0;
-pthread_barrier_t barrier;
-int inconst = 0;
-pthread_mutex_t mutex;
-pthread_cond_t cond;
-
 
 void printTreadError(int errCode, char * comment){
-	char *errorLine = strerror(errCode);
-	fprintf(stderr,"%s: %s\n",comment, errorLine);
+        char *errorLine = strerror(errCode);
+        fprintf(stderr,"%s: %s\n",comment, errorLine);
 }
 
 
 typedef struct data {
-	double res;
-	int id;
-	int N;
+        double res;
+        int id;
+        long long int top;
 } data;
 
-void finalize(int signo) { 
-	flag = 0;
+data *results = NULL;
+int flag = 1;
+int n = 0;
+
+void finalize(int signo) {
+        flag = 0;
 }
 
-
-
-
 void *work(void *arg) {
-	data *res = (data *)arg;
-	long long int i = 0;
-	long long int k = 0;
-	long long int j = res->id;
-	long long int max = 0;
-	int br = 0;
-	double a = 1; double b = 1;
-	res->top = 0;
-
-	while (flag) {
-		for (i = 0; i < ITER; ++i) {
-			a = (2*2*j + 1);
-			b = (2*(2*j+1) + 1);
-			res->res += (1/(a)) - (1/(b));
-			j += N;
-		}
-		if (!flag) break;
-		pthread_mutex_lock(&mutex);
-		++n;
-		if (n < alive) {
-			pthread_cond_wait(&cond,&mutex);
-		} else {
-			pthread_cond_broadcast(&cond);
-			n = 0;
-		}
-		pthread_mutex_unlock(&mutex);
-		//if (pthread_self() == 3) sleep(1);
-	}
-	pthread_mutex_lock(&mutex);
-	--alive;
-	pthread_cond_broadcast(&cond);
-	pthread_mutex_unlock(&mutex);
-	pthread_exit(NULL);
-
+        data *res = (data *)arg;
+        long long int i = 0;
+        long long int k = 0;
+        long long int j = res->id;
+        long long int max = 0;
+        int br = 0;
+        double a = 1; double b = 1;
+        res->top = 0;
+        while (flag) {
+                for (i = 0; i < ITER; ++i) {
+                        a = (2*2*j + 1);
+                        b = (2*(2*j+1) + 1);
+                        res->res += (1/(a)) - (1/(b));
+                        j += N;
+                }
+                if (!flag) break;
+                pthread_mutex_lock(&mutex);
+                ++n;
+                if (n < alive) {
+                        pthread_cond_wait(&cond,&mutex);
+                } else {
+                        pthread_cond_broadcast(&cond);
+                        n = 0;
+                }
+                pthread_mutex_unlock(&mutex);
+                if (pthread_self() == 3) sleep(1);
+        }
+        pthread_mutex_lock(&mutex);
+        --alive;
+        pthread_cond_broadcast(&cond);
+        pthread_mutex_unlock(&mutex);
+        pthread_exit(NULL);
 }
 
 
 int main(int argc, char **argv) {
-	pthread_t *thread = NULL;
+        pthread_t *thread = NULL;
 
-	double PI = 0;
-	
-	int error;
+        double PI = 0;
+        int i = 0;
+        if (argc < 2) {
+                perror("Too few arguments");
+                return 1;
+        }
+        int error;
+        N = atoi(argv[1]);
+        ITER = atoi(argv[2]);
 
-	if (argc < 2) {
-		perror("Too few arguments");
-		return INCORRECT_ARGS;
-	}
+        if (ITER <= 0) {
+                perror("Incorrect number of iterations");
+                return INCORRECT_ARGS;
+        }
 
-	N = atoi(argv[1]);
-
-	if (N <= 0) {
-		perror("Incorrect number of threads");
-		return INCORRECT_ARGS;
-	}
-	
-
-	
-	thread = (pthread_t*)malloc(sizeof(pthread_t)*N);
-	if (thread== NULL) {
-		perror("couldn't allocate memory");
-		return ALLOCATOR_ERROR;
-	}
-	
-	results = (data*)malloc(sizeof(data)*N);
-	if (results == NULL) {
-		perror("couldn't allocate memory");
-		return ALLOCATOR_ERROR;
-	}
-	
-	error = pthread_barrier_init(&barrier, NULL, N));
-	if (error!=ALL_RIGHT) {
-		printTreadError(error,"couldn't setup barrier");
-		return ERROR_BARRIER_CREATE;
-	}
-	error = pthread_mutex_init(&mutex, NULL);
-	if (error!=ALL_RIGHT) {
-		printTreadError(error,"couldn't create mutex");
-		return ERROR_MUTEX_CREATE;
-	}
-	error = pthread_cond_init(&cond, NULL);
-	if (error!=ALL_RIGHT) {
-		printTreadError(error,"couldn't create condition");
-		return ERROR_CONDITION_CREATE;
-	}
+        alive = N;
+        if (N <= 0) {
+                perror("Incorrect number of threads");
+                return 1;
+        }
 
 
-	for (i = 0; i < N; ++i) {
-		results[i].res = 0;
-		results[i].id = i;
-		error = pthread_create(&(thread[i]), NULL, &work, results + i);
-		if (error!=ALL_RIGHT) {
-			printTreadError(error,"couldn't create thread");
-			return ERROR_TREAD_CREATE;
-		}
-	}
 
-	signal(SIGINT, finalize);
-	for (i = 0; i < N; ++i) {
-		error = pthread_join(thread[i], NULL);
-		if (error != ALL_RIGHT) {
-			perror("couldn't join threads");
-			return ERROR_TREAD_JOIN;
-		}
-		PI += results[i].res;
-	}
-	
-	
-	error = pthread_barrier_destroy(&barrier));
-	if (error!=ALL_RIGHT) {
-		printTreadError(error,"couldn't destroy barrier");
-		return ERROR_BARRIER_DESTROY;
-	}
-	error = pthread_mutex_destroy(&mutex);
-	if (error!=ALL_RIGHT) {
-		printTreadError(error,"couldn't destroy mutex");
-		return ERROR_MUTEX_DESTROY;
-	}
-	error = pthread_cond_destroy(&cond);
-	if (error!=ALL_RIGHT) {
-		printTreadError(error,"couldn't destroy condition");
-		return ERROR_CONDITION_DESTROY;
-	}
-	
-	free(results);
-	PI *= 4;
-	printf("PI = %.8lf\n",PI);
-	
-	
-	pthread_exit(NULL);
+        thread = (pthread_t*)malloc(sizeof(pthread_t)*N);
+        if (thread== NULL) {
+                perror("couldn't allocate memory");
+                return ALLOCATOR_ERROR;
+        }
+
+        results = (data*)malloc(sizeof(data)*N);
+        if (results == NULL) {
+                perror("couldn't allocate memory");
+                return ALLOCATOR_ERROR;
+        }
+
+        error = pthread_barrier_init(&barrier, NULL, N);
+        if (error!=ALL_RIGHT) {
+                printTreadError(error,"couldn't setup barrier");
+                return ERROR_BARRIER_CREATE;
+        }
+        error = pthread_mutex_init(&mutex, NULL);
+        if (error!=ALL_RIGHT) {
+                printTreadError(error,"couldn't create mutex");
+                return ERROR_MUTEX_CREATE;
+        }
+        error = pthread_cond_init(&cond, NULL);
+        if (error!=ALL_RIGHT) {
+                printTreadError(error,"couldn't create condition");
+                return ERROR_CONDITION_CREATE;
+        }
+
+        for (i = 0; i < N; ++i) {
+                results[i].res = 0;
+                results[i].id = i;
+                error = pthread_create(&(thread[i]), NULL, &work, results + i);
+                if (error!=ALL_RIGHT) {
+                        printTreadError(error,"couldn't create thread");
+                        return ERROR_TREAD_CREATE;
+                }
+        }
+        signal(SIGINT, finalize);
+        for (i = 0; i < N; ++i) {
+                error = pthread_join(thread[i], NULL);
+                if (error != ALL_RIGHT) {
+                        perror("couldn't join threads");
+                        return ERROR_TREAD_JOIN;
+                }
+                PI += results[i].res;
+        }
+
+        error = pthread_barrier_destroy(&barrier);
+        if (error!=ALL_RIGHT) {
+                printTreadError(error,"couldn't destroy barrier");
+                return ERROR_BARRIER_DESTROY;
+        }
+        error = pthread_mutex_destroy(&mutex);
+        if (error!=ALL_RIGHT) {
+                printTreadError(error,"couldn't destroy mutex");
+                return ERROR_MUTEX_DESTROY;
+        }
+        error = pthread_cond_destroy(&cond);
+        if (error!=ALL_RIGHT) {
+                printTreadError(error,"couldn't destroy condition");
+                return ERROR_CONDITION_DESTROY;
+        }
+
+        free(results);
+        PI *= 4;
+        printf("PI = %.16lf\n",PI);
+        fflush(0);
+        pthread_exit(NULL);
 }
 
 
