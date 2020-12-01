3dc9d4d188dd59ead27356fab23860e29423fefc
sanches185 <sanches185@gmail.com>
2020-12-01 14:17:26 +0700

    fix

--- lab-13.c
+++ lab-13.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <semaphore.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define ERROR_TREAD_CREATE -1
 #define ERROR_TREAD_JOIN -2
@@ -20,19 +21,20 @@ void printTreadError(int errCode, char * comment) {
 sem_t semaphore1;
 sem_t semaphore2;
 
+
 void *childText(void *arg) {
 	int error;
 	for (int i = 0; i < 10; i++) {
-		error = sem_post(&semaphore2);
+		error = sem_wait(&semaphore2);
 		if (error != ALL_RIGHT) {
-			printTreadError(error, "error semaphore post");
+			printTreadError(error, "error semaphore wait");
 			exit(ERROR_SEMAPHORE_POST);
 		}
 		printf("Thread 1: string %d\n", i + 1);
 
-		error = sem_wait(&semaphore1);
+		error = sem_post(&semaphore1);
 		if (error != ALL_RIGHT) {
-			printTreadError(error, "error semaphore wait");
+			printTreadError(error, "error semaphore post");
 			exit(ERROR_SEMAPHORE_WAIT);
 		}
 
@@ -43,20 +45,21 @@ void *childText(void *arg) {
 void parentText() {
 	int error;
 	for (int i = 0; i < 10; i++) {
-		error = sem_post(&semaphore1);
+		error = sem_wait(&semaphore1);
 		if (error != ALL_RIGHT) {
-			printTreadError(error, "error semaphore post");
+			printTreadError(error, "error semaphore wait");
 			exit(ERROR_SEMAPHORE_POST);
 		}
 		printf("Thread 0: string %d\n", i + 1);
-		error = sem_wait(&semaphore2);
+		error = sem_post(&semaphore2);
 		if (error != ALL_RIGHT) {
-			printTreadError(error, "error semaphore wait");
+			printTreadError(error, "error semaphore post");
 			exit(ERROR_SEMAPHORE_WAIT);
 		}
 	}
 
 }
+
 
 int init_sems() {
 	int error;
@@ -104,6 +107,12 @@ int main() {
 	}
 	parentText();
 
+	error = pthread_join(thread, NULL);
+	if (error != ALL_RIGHT) {
+        printTreadError(error, "join thread error");
+		return ERROR_TREAD_JOIN;
+    }
+	
 	error = destroy_sems();
 	if (error != ALL_RIGHT) {
 		return ERROR_SEMAPHORE_DESTROY;
